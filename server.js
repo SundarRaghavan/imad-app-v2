763fac8906a927f479727fcdc9b75e33cdbaa133
Sundar Raghavan <sundarraghavan10@gmail.com>
2017-04-07 12:39:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ var articleOne ={
 };
 function createTemplate (data){
 var title= data.title;
-var.date= data.date;
-var.heading=data.heading;
-var content=data.content;
+var date= data.date;
+var heading= data.heading;
+var content= data.content;
 
 var htmlTemplate =`
 <html>
