a828802487e2642fad8d66770728b86309f2c3a9
Sundar Raghavan <sundarraghavan10@gmail.com>
2017-04-07 12:03:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one',function(req, res){
 });
 
 app.get('/article-two',function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'articletTwo.html'));  
+   res.sendFile(path.join(__dirname, 'ui', 'articlet-two.html'));  
 });
 
 app.get('/article-three',function(req, res){
